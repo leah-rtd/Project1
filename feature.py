d97b675b0bdeaceb9f60e8c27b37f9cb5200a059
leah <115782687+leah-rtd@users.noreply.github.com>
2025-06-09 08:42:29 +0000

    added basic modeling

--- feature.py
+++ feature.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.linear_model import LinearRegression
 def preproc(X):
     scaler = StandardScaler()
+    model = LinearRegression()
+    model.fit(X)
     return scaler.fit_transform(X)
